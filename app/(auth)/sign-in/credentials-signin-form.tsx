faa935cf5707169b8fc847039d62a01b5fee0845
Jubril <jubriloyebamiji@gmail.com>
2025-09-21 06:38:32 -0700

    fixed a bug in user auth

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -63,7 +63,7 @@ const CredentialsSignInForm = () => {
       )}
 
       <div className="text-sm text-center text-muted-foreground">
-        Don&apos;t have an account?{" "}
+        Don&apos;t have an account?
         <Link href="/sign-up" className="text-blue-500 hover:underline">
           Sign Up
         </Link>
