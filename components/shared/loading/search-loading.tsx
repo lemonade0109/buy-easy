f75be1ad395e7a5f6a87ed3ab593ee08556253af
Jubril <jubriloyebamiji@gmail.com>
2025-12-17 14:29:26 -0800

    added styling to fit mobile screen

--- components/shared/loading/search-loading.tsx
+++ components/shared/loading/search-loading.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const SearchLoading = () => {
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      {/* Sidebar Skeleton */}
-      <div className="filter-links">
+      {/* Sidebar Skeleton for md+ */}
+      <div className="hidden md:block filter-links">
         {/* Department Section */}
         <div className="mt-3 mb-2">
           <Skeleton className="h-7 w-32 mb-2" />
@@ -38,17 +38,30 @@ const SearchLoading = () => {
         </div>
       </div>
 
+      {/* Mobile Sidebar Skeleton */}
+      <div className="block md:hidden mb-4">
+        <div className="flex gap-2 mb-2">
+          <Skeleton className="h-6 w-20" />
+          <Skeleton className="h-6 w-20" />
+        </div>
+        <div className="flex gap-2 mb-2">
+          <Skeleton className="h-4 w-16" />
+          <Skeleton className="h-4 w-16" />
+          <Skeleton className="h-4 w-16" />
+        </div>
+      </div>
+
       {/* Main Content Skeleton */}
-      <div className="md:col-span-4 space-y-4">
+      <div className="md:col-span-4 space-y-4 w-full">
         {/* Filter/Sort Bar */}
-        <div className="flex-between flex-col md:flex-row my-4">
-          <Skeleton className="h-6 w-48" />
-          <Skeleton className="h-10 w-40" />
+        <div className="flex flex-col md:flex-row gap-2 my-4">
+          <Skeleton className="h-6 w-32 md:w-48" />
+          <Skeleton className="h-10 w-32 md:w-40" />
         </div>
 
         {/* Product Grid Skeleton */}
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {[...Array(12)].map((_, i) => (
+        <div className="grid grid-cols-2 gap-3 md:gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {[...Array(8)].map((_, i) => (
             <ProductCardLoading key={i} />
           ))}
         </div>
